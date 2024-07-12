d187f309b70e9255f5cb34e9851077f6678f43a1
Samantha Perry <samnperry@arizona.edu>
2024-07-12 11:41:10 +0300

    add: wobbly service interaction

--- microservice-a/src/main/java/cimet2/test/microservicea/service/ServiceA.java
+++ microservice-a/src/main/java/cimet2/test/microservicea/service/ServiceA.java
@@ -3,6 +3,11 @@ package cimet2.test.microservicea.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @Service
 public class ServiceA {
 
@@ -12,7 +17,15 @@ public class ServiceA {
         return restTemplate.getForObject("/service-b/endpoint", String.class);
     }
 
+    @CircuitBreaker(name = "serviceA", fallbackMethod = "fallbackForService")
+    @RateLimiter(name = "serviceA", fallbackMethod = "fallbackForService")
+    @Retry(name = "serviceA", fallbackMethod = "fallbackForService")
+    @Bulkhead(name = "serviceA", fallbackMethod = "fallbackForService")
     public String callServiceC() {
         return restTemplate.getForObject("/service-c/endpoint", String.class);
+    }
+
+    public String fallbackForCallServiceC(Exception e) {
+        return "Fallback response for Service C due to " + e.getMessage();
     }
 }
--- microservice-d/src/main/java/cimet2/test/microserviced/service/ServiceD.java
+++ microservice-d/src/main/java/cimet2/test/microserviced/service/ServiceD.java
@@ -3,12 +3,19 @@ package cimet2.test.microserviced.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class ServiceD {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @CircuitBreaker(name = "serviceD", fallbackMethod = "fallbackForService")
     public String callServiceB() {
         return restTemplate.getForObject("/service-b/call-d", String.class);
+    }
+
+    public String fallbackForCallServiceB(Exception e) {
+        return "Fallback response for Service B due to " + e.getMessage();
     }
 }
